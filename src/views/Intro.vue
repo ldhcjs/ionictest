0b490adfacb0df52ad57c0cd526b7a6d7cf8b41b
Tony Lee <ldhcjs@gmail.com>
2024-03-19 23:49:01 +0900

    [Tony] Added progress bar in intro.

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -6,13 +6,20 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+      <ion-progress-bar type="indeterminate"></ion-progress-bar>
       <ion-img src="https://ionic-docs-demo-v7.vercel.app/assets/madison.jpg"
-        alt="The Wisconsin State Capitol building in Madison, WI at night"/>
+        alt="The Wisconsin State Capitol building in Madison, WI at night" />
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonProgressBar } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { IonProgressBar },
+});
 </script>
