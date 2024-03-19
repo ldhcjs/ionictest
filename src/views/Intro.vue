9024e06ab73b976bb679a6bd952a2a6bc39c1600
Tony Lee <ldhcjs@gmail.com>
2024-03-19 23:33:00 +0900

    [Tony] Initial Commit for Ionic Vue

---
+++ src/views/Intro.vue
@@ -0,0 +1,18 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Intro Page</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-img src="https://ionic-docs-demo-v7.vercel.app/assets/madison.jpg"
+        alt="The Wisconsin State Capitol building in Madison, WI at night"/>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+</script>
